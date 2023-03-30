f95a863f26b995466cc53ebf96a7a292dc07f5a4
Aasif Ali <aasif.ali@knoldus.com>
2023-03-30 13:38:19 +0530

    added fallback methods

--- src/main/java/com/knoldus/resilience4jdemo/controller/TestController.java
+++ src/main/java/com/knoldus/resilience4jdemo/controller/TestController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class TestController {
@@ -17,7 +17,12 @@ public class TestController {
     UserService userService;
 
     @GetMapping("/users")
-    public List<User> getUsers() {
+    public ResponseEntity<List<User>> getUsers() {
         return userService.getUsers();
+    }
+
+    @GetMapping("/users/timelimiter")
+    public CompletableFuture<List<User>> getUsersTimeLimiter() {
+        return userService.getUsersTimeLimiter();
     }
 }
